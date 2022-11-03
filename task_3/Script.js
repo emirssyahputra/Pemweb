53e2f967e936cac84044cc46661c624241eb22e3
EMIRSSYAH PUTRA <emirssyah13@gmail.com>
2022-11-03 19:05:06 +0700

    update revisi :)

--- task_3/Script.js
+++ task_3/Script.js
@@ -1,16 +1,35 @@
+const a = document.getElementById("desk");
+a.innerHTML =
+  "Hai, nama saya Emirssyah Putra, umur saya 20 tahun, saya kuliah di Institut Teknologi Sumatera program studi Teknik Informatika Angkatan 20, saat ini saya berada di semester 5";
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+var acc = document.getElementsByClassName("accordion1");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 function tombol() {
-    const a = document.getElementById('judul'); 
-    a.innerHTML = 'Halooooo';
-
-    const b = document.getElementById('bawahjudul');
-    b.innerHTML = 'Perkenalkan :)';
-
-    const elemenH3 = document.createElement('h3');
-    const teksElemenH3 = document.createTextNode('Halo, nama saya Emirssyah Putra dengan NIM 120140169, saya kelas pemrograman web RB, ini tugas ketiga saya :)'); // Deklarasi Variabel untuk mengisi teks
-
-    elemenH3.appendChild(teksElemenH3); 
-
-    const cetak = document.getElementById('body');
-
-    cetak.appendChild(elemenH3);
+  alert("Terima Kasih Telah Submit :) ");
 }
